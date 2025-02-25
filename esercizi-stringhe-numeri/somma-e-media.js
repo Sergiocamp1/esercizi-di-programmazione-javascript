a1fb0391be3cf3801f19d5a2ee012417b7c515b2
Sergiocamp1 <149681668+Sergiocamp1@users.noreply.github.com>
2025-02-25 15:28:00 +0100

    Update somma-e-media.js
    esercizio somma media

--- esercizi-stringhe-numeri/somma-e-media.js
+++ esercizi-stringhe-numeri/somma-e-media.js
@@ -8,3 +8,23 @@
   
   http://www.imparareaprogrammare.it
 */
+
+//definiamo i numeri nelle variabili
+
+var n1 = 1;
+var n2 = 2;
+var n3 = 3;
+var n4 = 4;
+var n5 = 5;
+
+//definiamo la variabile somma
+
+var somma = n1 + n2 + n3 + n4 + n5;
+console.log(somma);
+
+// definiamo la variabile media
+
+var media = somma/5;
+console.log(media);
+
+//
